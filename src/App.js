35dc053e8b450c709a673d015180e599b6b293a2
Nicolai Ghenov <ghenovnikolai@gmail.com>
2023-04-19 14:23:17 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
-import {useEffect} from 'react';
+import { useEffect } from "react";
 
-const tg = window.Telegram.WebApp;
 function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
 
-    useEffect(() => {
-        tg.ready();
-    }, []);
-    const onClose = () => {
-        tg.close();
-    }
   return (
     <div className="App">
       <h1>Hello Bot</h1>
-      <button onClick={onClose}>Click Me</button>
     </div>
   );
 }
