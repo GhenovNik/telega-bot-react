99f906f652ec44a08416a2f149c6415cd812939e
Nicolai Ghenov <ghenovnikolai@gmail.com>
2023-04-23 19:08:53 -0700

    init(add)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { useEffect } from "react";
+import useTelegram from "./hooks/useTelegram";
 
 function App() {
+  const  {onToggleButton, tg } = useTelegram();
   useEffect(() => {
     tg.ready();
   }, []);
 
   return (
     <div className="App">
-      <h1>Hello Bot</h1>
+      <button onClick={onToggleButton}>Toggle Button</button>
     </div>
   );
 }
